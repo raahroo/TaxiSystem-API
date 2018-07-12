7dd3c1f23a1f00cd0068861020ea4b7e025b0612
Raissa Hohenester <raissa.developer@gmail.com>
2018-07-12 17:13:36 +0200

    Handling Errors and Project Setup Improved

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const carRoutes = require('./api/routes/cars');
 const passengerRoutes = require('./api/routes/passengers');
 const demandRoutes = require('./api/routes/demands');
 
+app.use(morgan('dev'));
+
 // Routes which should handle requests
 app.use('/cars', carRoutes);
 app.use('/passengers', passengerRoutes);
 app.use('/demands', demandRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
